854bf27d6ff147298a63ac9fc2991e2153928925
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-04 22:50:20 +0530

    remove some comment along with a import statement

--- App.js
+++ App.js
@@ -5,8 +5,6 @@ import Constants from 'expo-constants';
 // You can import from local files
 import FirstComp from './components/FirstComp';
 
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
 
 export default function App() {
   return (
