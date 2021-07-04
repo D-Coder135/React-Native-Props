ec3341f4a2287901a24e1c323a0c2c6d2856d94b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-05 00:16:49 +0530

    add the render() function inside the class

--- components/SecondComp.js
+++ components/SecondComp.js
@@ -3,5 +3,7 @@ import {View, Text, Image} from 'react-native';
 
 // This component is made using class. i.e Class Component.
 export default class SecondComp extends React.Component {
-    
+    render () {
+        
+    }
 }
