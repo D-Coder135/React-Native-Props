91d89cee21080ef60965a1d3b3a899ff9d36585f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-05 00:26:53 +0530

    copy paste the styles from the FirstComp to SecondComp

--- components/SecondComp.js
+++ components/SecondComp.js
@@ -16,3 +16,22 @@ export default class SecondComp extends React.Component {
           );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 24,
+    },
+    paragraph: {
+      margin: 24,
+      marginTop: 0,
+      fontSize: 14,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    logo: {
+      height: 128,
+      width: 128,
+    }
+  });
