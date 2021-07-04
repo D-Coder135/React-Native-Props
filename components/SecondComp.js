fe7b7bbc64678bc698b6ebe480e54e9109fd75e1
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-05 00:21:46 +0530

    add the return statement for the render() function

--- components/SecondComp.js
+++ components/SecondComp.js
@@ -4,6 +4,14 @@ import {View, Text, Image} from 'react-native';
 // This component is made using class. i.e Class Component.
 export default class SecondComp extends React.Component {
     render () {
-        
+        return (
+            <View style={styles.container}>
+              <Text style={styles.paragraph}>
+                {props.name}
+              </Text>
+              {/* <Image style={styles.logo} source={require('../assets/favicon.png')} /> */}
+              <Image style = {styles.logo} source = {{uri: props.pic}}/>
+            </View>
+          );
     }
 }
