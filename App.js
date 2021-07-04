2ba29b9a836e0c1a02add773a59b062f29704f7b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-04 22:29:43 +0530

    add some text inside the Text tag

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>
-        
+        React-Props
       </Text>
       <Card>
         <AssetExample />
