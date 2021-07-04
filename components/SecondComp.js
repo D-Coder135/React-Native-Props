8b945b4e50fdb66d939e3c8b9bb594b120ad0cc0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-05 00:16:18 +0530

    add a class SecondComp inside which extends React.Component

--- components/SecondComp.js
+++ components/SecondComp.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import {View, Text, Image} from 'react-native';
 
-//This component is made using class. i.e Class Component.
+// This component is made using class. i.e Class Component.
+export default class SecondComp extends React.Component {
+    
+}
